19392a31dd57dd7640cf71caf2568560b57a7451
x2loreeh <128195197+x2loreeh@users.noreply.github.com>
2025-01-10 18:34:06 +0100

    Update 5
    Added cover image, and now i'm trying to make a more enjoyable ui

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('searchButton').addEventListener('click', function() {
         const songTitle = document.getElementById('songInput').value;
         if (songTitle) {
@@ -22,12 +22,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function displayResults(data) {
         const resultsDiv = document.getElementById('results');
+
         if (!resultsDiv) {
             console.error('L\'elemento #results non è stato trovato nel DOM.');
             return;
         }
-
-        resultsDiv.innerHTML = ''; 
+        resultsDiv.innerHTML = '';
 
         if (data.data.length === 0) {
             resultsDiv.innerHTML = 'Nessun risultato trovato.';
@@ -38,12 +38,27 @@ document.addEventListener('DOMContentLoaded', function() {
             const trackElement = document.createElement('div');
             trackElement.classList.add('track-result');
 
-            trackElement.innerHTML = `
-                <h3>${track.title}</h3>
-                <p>Artista: ${track.artist.name}</p>
-                <a href="${track.link}" target="_blank">Ascolta su Deezer</a>
-                <br><br>
-            `;
+            const imgElement = document.createElement('img');
+            imgElement.src = track.album.cover_medium;
+            imgElement.alt = `${track.title} cover`;
+            imgElement.style.width = '125px'; 
+
+            const titleDiv = document.createElement('div');
+            titleDiv.classList.add('track-title');
+            titleDiv.innerHTML = `<h3>${track.title}</h3>`;
+
+            const artistDiv = document.createElement('div');
+            artistDiv.classList.add('track-artist');
+            artistDiv.innerHTML = `<p>Artista: ${track.artist.name}</p>`;
+
+            const linkDiv = document.createElement('div');
+            linkDiv.classList.add('track-link');
+            linkDiv.innerHTML = `<a href="${track.link}" target="_blank">Ascolta su Deezer</a>`;
+
+            trackElement.appendChild(imgElement);
+            trackElement.appendChild(titleDiv);
+            trackElement.appendChild(artistDiv);
+            trackElement.appendChild(linkDiv);
 
             resultsDiv.appendChild(trackElement);
         });
