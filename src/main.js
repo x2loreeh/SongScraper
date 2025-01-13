4f45602d72f55b3e33cedfb7e12a4578b014159a
x2loreeh <128195197+x2loreeh@users.noreply.github.com>
2025-01-13 18:37:29 +0100

    Update 12
    Added DiscordRPC

--- src/main.js
+++ src/main.js
@@ -1,29 +1,46 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow} = require('electron');
+const RPC = require('discord-rpc'); 
+const clientId = '1328057808523890722'; 
 
-function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    icon: path.join(__dirname, "assets", "logo.png"),
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-   mainWindow.loadFile('./src/index.html');
-   mainWindow.webContents.openDevTools();
-}
+let mainWindow;
+let rpc;
 
-app.on('ready', createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        icon: "./src/assets/logo.png"
+    });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+
+  
+    mainWindow.loadFile('./src/index.html');
+
+    rpc = new RPC.Client({ transport: 'ipc' });
+
+    rpc.on('ready', () => {
+        console.log('Discord RPC is ready!');
+        rpc.setActivity({
+            details: 'SongScraper',
+            state: 'Listening music on SongScraper',
+            startTimestamp: Date.now(),
+            largeImageKey: 'logo',
+            largeImageText: 'https://github.com/x2loreeh/SongScraper',
+            smallImageKey: 'icon',
+            smallImageText: 'https://github.com/x2loreeh/SongScraper',
+            instance: true,
+            buttons : [{label: "Ascolta con me", url: "https://github.com/x2loreeh/SongScraper"}] 
+        });
+    });
+
+    rpc.login({ clientId });
+    app.on('before-quit', () => {
+        rpc.destroy();
+    });
+
+  
 });
