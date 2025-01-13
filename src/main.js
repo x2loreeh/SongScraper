46be70fa333fa950b25304e5cc3bcc6015b0f33a
x2loreeh <128195197+x2loreeh@users.noreply.github.com>
2025-01-13 18:43:17 +0100

    Update 13
    Added a custom menu, with labels and shortcuts

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow} = require('electron');
+const { app, BrowserWindow, Menu, shell } = require('electron');
 const RPC = require('discord-rpc'); 
 const clientId = '1328057808523890722'; 
 
@@ -14,11 +14,58 @@ app.on('ready', () => {
             nodeIntegration: true
         },
         icon: "./src/assets/logo.png"
-    });
+    },
+);
 
 
-  
+const menuSS = [
+    {
+      label: "SongScraper",
+      submenu: [
+        {
+          label: "Changelogs",
+          accelerator: process.platform === "darwin" ? "Command+M" : "Ctrl+M",
+          click: () => {
+            shell.openExternal("https://github.com/x2loreeh/SongScraper");
+          },
+        },
+        {
+          label: "Wiki",
+          accelerator: process.platform === "darwin" ? "Command+?" : "Ctrl+?",
+          click: () => {
+            shell.openExternal("https://github.com/x2loreeh/SongScraper");
+          },
+        },
+        {
+          role: "reload",
+        },
+        {
+          label: "Quit",
+          accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+W",
+          click() {
+            app.quit();
+          },
+        },
+      ],
+    },
+    {
+      label: "Support",
+      submenu: [
+        {
+          label: "Donations",
+          click: () => {
+            shell.openExternal("https://paypal.me/x2loreeh");
+          },
+        },
+      ],
+    },
+  ];
+
+    const menuSongScraper = Menu.buildFromTemplate(menuSS);
+    Menu.setApplicationMenu(menuSongScraper);
     mainWindow.loadFile('./src/index.html');
+
+    
 
     rpc = new RPC.Client({ transport: 'ipc' });
 
@@ -41,6 +88,5 @@ app.on('ready', () => {
     app.on('before-quit', () => {
         rpc.destroy();
     });
-
   
 });
